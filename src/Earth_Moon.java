c0e4c3db9dea4c27e137eb2bc7d4fba4f659c82f
Mikhail Khlopkov <hlopkovmihail91@gmail.com>
2025-06-10 20:14:34 +0300

    add Earth_Moon

--- src/Earth_Moon.java
+++ src/Earth_Moon.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
-public class Earth_Moon {
+/*
+  Программа для расчёты фактического веса на луне
+  Вводные: Сила тяжести на луне 17% от земной
+ */
+class Earth_Moon {
+    public static void main(String[] args) {
+        double myWeight = 105;
+        double myWeightOnTheMoon = myWeight * 17 / 100;
+        System.out.println("Мой вес на луне составляет " + myWeightOnTheMoon + " килограмм");
+    }
 }
