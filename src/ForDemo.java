972dcc4148e5f2df68831e30aa9d469e5b0d9f61
Mikhail Khlopkov <hlopkovmihail91@gmail.com>
2025-06-10 18:49:07 +0300

    add ForDemo

---
+++ src/ForDemo.java
@@ -0,0 +1,13 @@
+/* Демонстрация использования цикла for.
+   Назовите этот файл ForDemo.java.
+ */
+class ForDemo {
+    public static void main(String[] args) {
+        int count;
+//        for(count = 0; count < 5; count = count++)
+        for(count = 0; count < 5; count = count+1)
+            System.out.println("Значение count: " + count);
+        System.out.println("Готово!");
+
+    }
+}
